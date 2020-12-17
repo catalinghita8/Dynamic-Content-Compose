a3db49d3a6726589243905339720dc22d679c545
Catalin Ghita <cghita@airtouchmedia.com>
2020-12-17 19:22:44 +0200

    Attempt to see recomposition

--- app/src/main/java/com/codingtroops/dynamiccontentexample/MainActivity.kt
+++ app/src/main/java/com/codingtroops/dynamiccontentexample/MainActivity.kt
@@ -2,16 +2,21 @@ package com.codingtroops.dynamiccontentexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import com.codingtroops.dynamiccontentexample.ui.DynamicContentExampleTheme
 
-val namesList: List<String> = listOf(
+val namesList: ArrayList<String> = arrayListOf(
         "John",
         "Michael",
         "Andrew",
@@ -29,16 +34,27 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun GreetingList(names: List<String>) {
-    Column {
+    Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         for(name in names) {
             Greeting(name = name)
+        }
+
+        Button(onClick = { namesList.add("New name") }) {
+            Text("Add new name")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+            text = "Hello $name!",
+            style = MaterialTheme.typography.h4
+    )
 }
 
 @Preview(showBackground = true)
