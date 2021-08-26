481f6390473110bc8b19cf521929cb85eb7988d8
Catalin Ghita <cghita@airtouchmedia.com>
2021-08-26 11:52:29 +0300

    1.0.1 update

--- app/src/main/java/com/codingtroops/dynamiccontentexample/MainActivity.kt
+++ app/src/main/java/com/codingtroops/dynamiccontentexample/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.codingtroops.dynamiccontentexample
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
-import com.codingtroops.dynamiccontentexample.ui.DynamicContentExampleTheme
 
 val namesList: List<String> = listOf(
         "John",
