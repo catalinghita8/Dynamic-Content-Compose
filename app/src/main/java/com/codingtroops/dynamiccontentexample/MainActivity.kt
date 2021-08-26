b20453d3e04448586f6085a19459539f4a572153
Catalin Ghita <cghita@airtouchmedia.com>
2021-08-26 11:53:55 +0300

    Attempt to see recomposition -  1.0.1 update

--- app/src/main/java/com/codingtroops/dynamiccontentexample/MainActivity.kt
+++ app/src/main/java/com/codingtroops/dynamiccontentexample/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.codingtroops.dynamiccontentexample
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
-import com.codingtroops.dynamiccontentexample.ui.DynamicContentExampleTheme
 
 val namesList: ArrayList<String> = arrayListOf(
         "John",
@@ -27,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-          GreetingList(names = namesList)
+            GreetingList(names = namesList)
         }
     }
 }
@@ -39,7 +37,7 @@ fun GreetingList(names: List<String>) {
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        for(name in names) {
+        for (name in names) {
             Greeting(name = name)
         }
 
